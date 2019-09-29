bea721567c2a46fabf0659b2775ac52c4c672716
einstein8612 <joey0312004@gmail.com>
2019-09-29 20:20:10 +0200

    Added drop listener to keep players from loosing their menu item

---
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/listeners/PlayerDropListener.java
@@ -0,0 +1,28 @@
+package net.worldofsurvival.wosskyblock.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+import net.worldofsurvival.wosskyblock.items.MainItems;
+import net.worldofsurvival.wosskyblock.utils.Common;
+
+public class PlayerDropListener implements Listener {
+	
+	private Common common;
+	private MainItems items;
+	
+	public PlayerDropListener(Common common, MainItems items) {
+		this.common = common;
+		this.items = items;
+	}
+
+	@EventHandler
+	public void onDrop(PlayerDropItemEvent event) {
+		if (event.getItemDrop().getItemStack().equals(items.menu())) {
+			event.setCancelled(true);
+			common.tell(event.getPlayer(), "&cYou can't drop the menu item!");
+		}
+	}
+	
+}
