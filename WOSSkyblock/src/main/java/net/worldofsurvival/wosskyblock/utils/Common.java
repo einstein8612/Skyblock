1f95df532ecd34753fc81c97652cc1f06870c879
einstein8612 <joey0312004@gmail.com>
2019-10-02 18:37:16 +0200

    Removed the hotbar class in favour of more efficient code, added Common#logError aswell to log errors

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/utils/Common.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/utils/Common.java
@@ -2,8 +2,11 @@ package net.worldofsurvival.wosskyblock.utils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public final class Common {
 
@@ -22,7 +25,15 @@ public final class Common {
 	}
 	
 	public void log(String message) {
-		Bukkit.getConsoleSender().sendMessage("[WOSSkyblock] " + this.colorize(message));
+		Bukkit.getConsoleSender().sendMessage(this.colorize("[WOSSkyblock] " + message));
+	}
+	
+	public void logError(String message) {
+		Bukkit.getConsoleSender().sendMessage(this.colorize("&4[&cWOSSkyblock&4] &c" + message));
+	}
+	
+	public void sendActionBar(Player player, String message) {
+		player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
 	}
 	
 }
