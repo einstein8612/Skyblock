bd3964b17d2698c66144caf664799726adfbadbf
einstein8612 <joey0312004@gmail.com>
2019-10-10 19:45:01 +0200

    Made it make the biome plains

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/VoidGenerator.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/VoidGenerator.java
@@ -3,13 +3,14 @@ package net.worldofsurvival.wosskyblock.generators;
 import java.util.Random;
 
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 
 public final class VoidGenerator extends ChunkGenerator {
 	
 	@Override
     public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
+		biome.setBiome(chunkX, chunkZ, Biome.PLAINS);
 		return createChunkData(world);
     }
-	
 }
