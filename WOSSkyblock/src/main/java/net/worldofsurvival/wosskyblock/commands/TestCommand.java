e3900e0489b26e884ff73950508a33d826acdc8d
einstein8612 <joey0312004@gmail.com>
2019-10-10 19:44:25 +0200

    Update TestCommand.java

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/commands/TestCommand.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/commands/TestCommand.java
@@ -1,34 +1,29 @@
 package net.worldofsurvival.wosskyblock.commands;
 
-import java.util.Set;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 import net.worldofsurvival.wosskyblock.utils.DataManager;
 
 public class TestCommand extends Command {
-	
+
 	private DataManager dm;	
 	public TestCommand(DataManager dm) {
 		super("test");
-		
-		
+
+
 		this.dm = dm;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-		FileConfiguration skyblocks = dm.getSkyblocksFile();
-		if (skyblocks.getConfigurationSection("Skyblocks") == null) {
-			skyblocks.createSection("Skyblocks");
-		}
-		Set<String> keys = skyblocks.getConfigurationSection("Skyblocks").getKeys(false);
-		int count = keys.size();
-		
-		sender.sendMessage(String.valueOf(count));
+		Player player = (Player) sender;
+		dm.createPlayerData(player);
+		player.sendMessage("Reset playerdata for yourself");
+
+
 		return true;
 	}
 }
