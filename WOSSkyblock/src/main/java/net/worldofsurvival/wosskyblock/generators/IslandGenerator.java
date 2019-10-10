38b9e79591f49116b95dbde5f58168a20fc3ce2d
einstein8612 <joey0312004@gmail.com>
2019-10-10 19:44:46 +0200

    Changed to actually work

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/IslandGenerator.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/IslandGenerator.java
@@ -2,6 +2,7 @@ package net.worldofsurvival.wosskyblock.generators;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.TreeType;
 import org.bukkit.block.Block;
 
 public final class IslandGenerator {
@@ -9,10 +10,14 @@ public final class IslandGenerator {
 	public IslandGenerator () {
 		//TODO: empty
 	}
-	
+
 	public void generateClassic(final Location location) {
 		Block block = location.getBlock();
 		block.setType(Material.BEDROCK);
+
+		location.setY(location.getY()+1);
+		location.getBlock().setType(Material.DIRT);
+		location.setY(location.getY()+1);
+		location.getWorld().generateTree(location, TreeType.TREE);
 	}
-	
 }
