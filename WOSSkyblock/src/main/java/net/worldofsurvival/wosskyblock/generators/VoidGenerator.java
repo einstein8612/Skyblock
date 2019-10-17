a32c65cee3ce4703fd410820f83ad95de5dbfe94
einstein8612 <joey0312004@gmail.com>
2019-10-17 17:47:23 +0200

    Added biome setting

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/VoidGenerator.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/generators/VoidGenerator.java
@@ -7,10 +7,15 @@ import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 
 public final class VoidGenerator extends ChunkGenerator {
-	
+
 	@Override
-    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
-		biome.setBiome(chunkX, chunkZ, Biome.PLAINS);
-		return createChunkData(world);
-    }
+	public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
+		ChunkData chunk = createChunkData(world);
+		for (int x = 0; x < 16; x++) {
+			for (int z = 0; z < 16; z++) {
+				biome.setBiome(x, z, Biome.PLAINS);
+			}
+		}
+		return chunk;
+	}
 }
