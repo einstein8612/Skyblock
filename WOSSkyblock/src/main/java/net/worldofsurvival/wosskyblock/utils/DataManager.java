5d81eb06ab13eca7c3ac51eb993ee406475ec050
einstein8612 <joey0312004@gmail.com>
2019-10-17 17:49:43 +0200

    Added first default schematic

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/utils/DataManager.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/utils/DataManager.java
@@ -3,6 +3,7 @@ package net.worldofsurvival.wosskyblock.utils;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -10,56 +11,63 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public final class DataManager {
-	
+
 	private File dataFolder;
-	
+	private File schemFolder;
+
 	private File configFile;
 	private FileConfiguration config;
-	
+
 	private File skyblocksFile;
 	private FileConfiguration skyblocks;
-	
+
 	private Plugin plugin;
-	
+
 	public void setup(Plugin plugin) {
 		this.plugin = plugin;
-		
+
 		this.dataFolder = new File(plugin.getDataFolder(), "playerdata");
 		this.configFile = new File(plugin.getDataFolder(), "config.yml");
 		this.skyblocksFile = new File(plugin.getDataFolder(), "skyblocks.yml");
-		
-		//MAKE DATA FOLDER
+
+		this.schemFolder = new File(plugin.getDataFolder(), "schematics");
+
+		// MAKE DATA FOLDER
 		if (!plugin.getDataFolder().exists()) {
 			plugin.getDataFolder().mkdir();
 		}
-		
+
 		if (!dataFolder.exists()) {
 			dataFolder.mkdir();
 		}
-		
+
+		if (!schemFolder.exists()) {
+			schemFolder.mkdir();
+			plugin.saveResource("schematics/classic.schem", false);
+		}
+
 		if (!configFile.exists()) {
 			plugin.saveResource("config.yml", false);
 		}
-		
+
 		if (!skyblocksFile.exists()) {
 			plugin.saveResource("skyblocks.yml", false);
 		}
-		
-		
+
 		config = YamlConfiguration.loadConfiguration(configFile);
 		skyblocks = YamlConfiguration.loadConfiguration(skyblocksFile);
-		
+
 	}
-	
+
 	public File getPlayerFile(Player player) {
 		File file = new File(plugin.getDataFolder(), "playerdata/" + player.getUniqueId().toString() + ".yml");
 		return file;
 	}
-	
+
 	public FileConfiguration getSkyblocksFile() {
 		return skyblocks;
 	}
-	
+
 	public void saveSkyblocks() {
 		try {
 			skyblocks.save(skyblocksFile);
@@ -68,7 +76,7 @@ public final class DataManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void createPlayerData(Player player) {
 		File file = new File(plugin.getDataFolder(), "playerdata/" + player.getUniqueId().toString() + ".yml");
 		FileConfiguration data = YamlConfiguration.loadConfiguration(file);
@@ -78,8 +86,7 @@ public final class DataManager {
 		data.set("hasIsland", false);
 		data.set("islandResets", 3);
 		data.set("maxIsland", 125);
-		ArrayList<String> teamMates = new ArrayList<String>();
-		data.set("teamMates", teamMates);
+		data.set("teamMates", new ArrayList<UUID>());
 		data.set("teamLeader", "");
 		try {
 			data.save(file);
@@ -88,9 +95,16 @@ public final class DataManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public FileConfiguration config() {
 		return config;
 	}
-	
+
+	public FileConfiguration getTest() {
+		File file = new File(plugin.getDataFolder(), "test.yml");
+		FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+		return config;
+	}
+
 }
