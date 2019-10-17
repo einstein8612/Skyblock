412b5b51af0e936c822c9ab7d818162bc0de8c9f
einstein8612 <joey0312004@gmail.com>
2019-10-17 17:47:00 +0200

    Added pasting for testing

--- WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/commands/TestCommand.java
+++ WOSSkyblock/src/main/java/net/worldofsurvival/wosskyblock/commands/TestCommand.java
@@ -1,28 +1,61 @@
 package net.worldofsurvival.wosskyblock.commands;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import net.worldofsurvival.wosskyblock.generators.IslandGenerator;
 import net.worldofsurvival.wosskyblock.utils.DataManager;
+import net.worldofsurvival.wosskyblock.utils.IslandMethods;
 
 public class TestCommand extends Command {
 
-	private DataManager dm;	
-	public TestCommand(DataManager dm) {
+	private DataManager dm;
+	private HashMap<Player, IslandMethods> playerData;
+	private IslandGenerator generator;
+
+	public TestCommand(DataManager dm, HashMap<Player, IslandMethods> playerData, IslandGenerator generator) {
 		super("test");
 
-
+		this.playerData = playerData;
+		this.generator = generator;
 		this.dm = dm;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-		Player player = (Player) sender;
-		dm.createPlayerData(player);
-		player.sendMessage("Reset playerdata for yourself");
+		
+		if (args[0].equalsIgnoreCase("paste")) {
+			Player player = (Player) sender;
+			generator.generateIsland(player.getLocation(), "worldChunk");
+		}
+		
+		if (args[0].equalsIgnoreCase("reset")) {
+			Player player = (Player) sender;
+			dm.createPlayerData(player);
+			player.sendMessage("Reset playerdata for yourself");
+			return true;
+		}
 
+		if (args[0].equals("testing")) {
+			IslandMethods sData = playerData.get(sender);
+			FileConfiguration conf = dm.getTest();
+
+			sData.getConfig().set("islandMiddle", conf.get("islandMiddle"));
+
+			try {
+				sData.saveData((Player) sender);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 
 		return true;
 	}
